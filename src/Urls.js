762a9eb7073a63461a41690953b1e35d81dadc14
inputstudios <Hitmanki@yandex.ru>
2024-04-18 10:08:01 +0400

    added a link to render.com for authentication

--- src/Urls.js
+++ src/Urls.js
@@ -1,1 +1,1 @@
-export const baseurl = `https://mern-blog-server-2xk1.onrender.com/`;
+export const baseUrl = `https://mern-blog-server-2xk1.onrender.com`;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -8,6 +8,7 @@ import {
    signInFailure,
 } from "../redux/user/userSlice";
 import OAuth from "../components/OAuth";
+import { baseUrl } from "../Urls";
 
 export default function SignIn() {
    const [formData, setFormData] = useState({});
@@ -24,7 +25,7 @@ export default function SignIn() {
       }
       try {
          dispatch(signInStart());
-         const res = await fetch("/api/auth/signin", {
+         const res = await fetch(`${baseUrl}/api/auth/signin`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
