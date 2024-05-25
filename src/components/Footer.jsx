5463bd115d6ba29a88eeb306c333a8334bbd9f06
Андрей Трепалин <Hitmanki@yandex.ru>
2024-05-26 01:21:58 +0400

    Updated footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom";
 import { BsGithub, BsDribbble, BsYoutube } from "react-icons/bs";
 import { SlSocialVkontakte } from 'react-icons/sl';
 import { BiLogoFigma } from 'react-icons/bi';
+import { Switch } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { changeLanguage } from "../redux/language/languageSlice";
 
-export default function FooterCom() {
+export default function FooterCom({ languages }) {
+   const dispatch = useDispatch();
+   const { currentLanguage } = useSelector((state) => state.language);
+
+   const handleLanguageChange = (newLanguage) => {
+      dispatch(changeLanguage(newLanguage));
+   };
+
    return (
-      <Footer container className="border border-t-8 border-teal-500">
+      <Footer container className="border border-t-8 border-teal-500" id="footer">
          <div className="w-full max-w-7xl mx-auto">
             <div className="grid w-full justify-between sm:flex md:grid-cols-1">
                <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-4 sm:gap-6">
@@ -106,18 +116,43 @@ export default function FooterCom() {
             </div>
             <Footer.Divider />
             <div className="w-full sm:flex sm:items-center sm:justify-between">
-               <Footer.Copyright
-                  className="text-xs"
-                  href="#"
-                  by="Input Studios"
-                  year={new Date().getFullYear()}
-               />
-               <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center text-xs">
+               <div className="flex items-center">
+                  <div className="relative">
+                     <Switch
+                        className="relative"
+                        checked={currentLanguage === 'en'}
+                        onChange={(event) => handleLanguageChange(event.target.checked ? 'en' : 'ru')}
+                        sx={{
+                        '& .MuiSwitch-switchBase.Mui-checked': {
+                           '& .flag-image': {
+                              backgroundImage: 'url(https://i.ibb.co/LtPNm0n/US.png)',
+                           },
+                        },
+                        '& .MuiSwitch-switchBase': {
+                           '& .flag-image': {
+                              backgroundImage: 'url(https://i.ibb.co/489wXn1/RU.png)',
+                           },
+                        },
+                        }}
+                     />
+                  </div>
+                  <span className="flag-image absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 rounded-full overflow-hidden"></span>
+                  <span className="w-16 text-center">
+                     {currentLanguage === 'en' ? 'English' : 'Русский'}
+                  </span>
+               </div>
+               <div className="flex flex-wrap gap-6 sm:mt-0 mt-4 sm:justify-center text-xs">
                   <Link to="/contacts" className="dark:text-gray-400 hover:underline">Contact us Input Studios</Link>
                   <Link to="/privacy" className="dark:text-gray-400 hover:underline">Privacy</Link>
                   <Link to="/terms-of-use" className="dark:text-gray-400 hover:underline">Terms of use</Link>
                   <Link to="/contact" className="dark:text-gray-400 hover:underline">Trademarks</Link>
                   <Link to="/contact" className="dark:text-gray-400 hover:underline">About our ads</Link>
+                  <Footer.Copyright
+                     className="text-xs text-black"
+                     href="#"
+                     by="Input Studios"
+                     year={new Date().getFullYear()}
+                  />
                </div>
             </div>
          </div>
