cfd8d289b47ec9ef3ebe7c725434e2d403e99dad
Андрей Трепалин <Hitmanki@yandex.ru>
2024-05-26 01:25:19 +0400

    Changed the design of the registration and login pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -46,15 +46,14 @@ export default function SignIn() {
       <div className="min-h-screen mt-20">
          <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
             {/* left */}
-            <div className="flex-1">
+            <div className="flex-1 max-w-sm">
                <Link to="/" className="font-bold dark:text-white text-4xl">
                   <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
                      Input Studios
                   </span>
-                  Blog
                </Link>
                <p className="text-sm mt-5">
-                  This a demo project. You can sign in with your email and password or with Goolgle.
+                  This a demo project. You can sign in with <br />your email and password or with Google.
                </p>
             </div>
             {/* right */}
@@ -95,7 +94,7 @@ export default function SignIn() {
                   <OAuth />
                </form>
                <div className="flex gap-2 text-sm mt-5">
-                  <span>Dont have an account?</span>
+                  <span>Don&apos;t have an account?</span>
                   <Link to="/sign-up" className="text-blue-500">
                      Sign Up
                   </Link>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -47,10 +47,9 @@ export default function SignUp() {
                   <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
                      Input Studios
                   </span>
-                  Blog
                </Link>
                <p className="text-sm mt-5">
-                  This a demo project. You can sign up with your email and password or with Goolgle.
+                  This a demo project. You can sign up with <br />your email and password or with Goolgle.
                </p>
             </div>
             {/* right */}
