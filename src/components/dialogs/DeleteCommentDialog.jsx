9caf5cf8ea7312a1bfcb00ff6ae92e078c0945a9
Андрей Трепалин <Hitmanki@yandex.ru>
2024-11-18 21:55:52 +0400

    Added button to close the dialog and close outside the dialog

--- src/components/dialogs/DeleteCommentDialog.jsx
+++ src/components/dialogs/DeleteCommentDialog.jsx
@@ -1,43 +1,59 @@
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import PropTypes from 'prop-types';
 import { useTranslation } from "react-i18next";
+import { RiCloseLine } from 'react-icons/ri';
 
 const DeleteCommentDialog = ({ show, onClose, onConfirm }) => {
-  const { t } = useTranslation();
-    if (!show) return null;
+   const { t } = useTranslation();
+   if (!show) return null;
 
-    return (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 shadow-sm">
-            <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 w-96 border-dialog border-gray-700">
-                <div className="text-center">
-                    <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-                    <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
-                      {t("dialogs:sure_want_delete_comment")}
-                    </h3>
-                    <div className="flex justify-center gap-4">
-                        <button
-                          className="bg-red-500 text-white rounded px-4 py-2 hover:bg-red-600 shadow-md"
-                          onClick={onConfirm}
-                        >
-                          {t("dialogs:yes_im_sure")}
-                        </button>
-                        <button
-                          className="bg-gray-300 text-gray-800 rounded px-4 py-2 hover:bg-gray-400 shadow-md"
-                          onClick={onClose}
-                        >
-                          {t("dialogs:no_cancel")}
-                        </button>
-                    </div>
-                </div>
+   const handleOverlayClick = (e) => {
+      if (e.target === e.currentTarget) {
+         onClose();
+      }
+   };
+
+   return (
+      <div 
+         className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 shadow-sm"
+         onClick={handleOverlayClick}
+      >
+         <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 w-96 border-dialog border-gray-700 relative">
+            <button 
+               onClick={onClose}
+               className="absolute top-3 right-3 rounded-md"
+            >
+               <RiCloseLine size={24} className="rounded-md hover:translate-y-[-3px] transition-transform duration-200 hover:bg-gray-600" />
+            </button>
+            <div className="text-center">
+               <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+               <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+                  {t("dialogs:sure_want_delete_comment")}
+               </h3>
+               <div className="flex justify-center gap-4">
+                  <button
+                     className="bg-red-500 text-white rounded-md px-4 py-2 hover:bg-red-600 shadow-md"
+                     onClick={onConfirm}
+                  >
+                     {t("dialogs:yes_im_sure")}
+                  </button>
+                  <button
+                     className="bg-gray-600 rounded-md px-4 py-2 hover:bg-gray-700 shadow-md"
+                     onClick={onClose}
+                  >
+                     {t("dialogs:no_cancel")}
+                  </button>
+               </div>
             </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 DeleteCommentDialog.propTypes = {
-  show: PropTypes.string.isRequired,
-  onClose: PropTypes.string.isRequired,
-  onConfirm: PropTypes.string.isRequired,
+   show: PropTypes.bool.isRequired,
+   onClose: PropTypes.func.isRequired,
+   onConfirm: PropTypes.func.isRequired,
 };
 
 export default DeleteCommentDialog;
